277bf9d8ee2ffa9f5953037b7043e61bedf600c0
CharlieThunkman <ThunkmanCharlie@gmail.com>
2020-12-26 11:19:06 -0500

    Reaction Roles Updated
    Still has issues with editing text when multiple messages are in the chat bar.

--- FirstTimeRun/lang_claim.js
+++ FirstTimeRun/lang_claim.js
@@ -1,92 +1,58 @@
-const { Guild } = require('discord.js');
-const messager = require('./messager');
-
-module.exports = client => {
-    const channelID = '491759340911591424';
-    index = 2;
-
-    const getEmoji = emojiName => client.emojis.cache.find(emoji => emoji.name === emojiName)
-
-    const emojiText = {
-        en: 'React with this flag to speak in this language for the respective channels.',
-        es: 'Reacciona con esta bandera para hablar en este idioma por los canales respectivos.',
-        pt: 'Reaja com este sinalizador para falar neste idioma para os respectivos canais.',
-        ko: '이 플래그로 반응하여 각 채널에 대해이 언어로 말합니다.',
-        ja: 'このフラグに反応して, それぞれのチャネルでこの言語で話します。',
-        ch: '对此标志做出反应, 以该语言在各个频道中讲话。',
-        de: 'Reagieren Sie mit diesem Flag, um in dieser Sprache für die jeweiligen Kanäle zu sprechen.',
-        fr: 'Réagissez avec ce drapeau pour parler dans cette langue pour les canaux respectifs.',
+const addReactions = (message, reactions) => {
+    message.react(reactions[0])
+    reactions.shift();
+    if (reactions.length > 0) {
+        setTimeout(() => addReactions(message, reactions), 750);
     }
-    const emojiRole = ['-EN-', '-ES-', '-PT-', '-KO-', '-JA-', '-CH-', '-DE-', '-FR-']
+}
 
-    const reactions = ['🇺🇸', '🇪🇸', '🇵🇹', '🇰🇷', '🇯🇵', '🇨🇳', '🇩🇪', '🇫🇷'];
+module.exports = async (client, id, text, reactions, index) => {
+    const channel = await client.channels.fetch(id);
 
-    const handelReaction = async (reaction, user, add) => {
-        if (user.id === "790173314995847189") { return; }
-        if (reaction.message.partial) await reaction.message.fetch();
-        if (reaction.partial) await reaction.fetch();
-        if (!reaction.message.guild) return;
-
-        const reactors = ['🇺🇸', '🇪🇸', '🇵🇹', '🇰🇷', '🇯🇵', '🇨🇳', '🇩🇪', '🇫🇷'];
-        const emoji = reaction.emoji.name;
-        // console.log(reactors);
-        const { guild } = reaction.message;
-        let roleIndex = 0;
-        let frIndex = -1;
-        for (const variable in reactors) {
-            // console.log(reactors[variable]);
-            // console.log(emoji);
-            if (reactors[variable] === emoji) {
-                frIndex = roleIndex;
-            }
-            roleIndex++;
-        }
-        console.log(frIndex)
-        if (frIndex === -1) return;
-        console.log('found flag');
-        const roleName = emojiRole[frIndex];
-        console.log(roleName);
-        const role = guild.roles.cache.find(role => role.name === roleName);
-        const member = guild.members.cache.find(member => member.id === user.id);
-        if (add) {
-            member.roles.add(role);
+    channel.messages.fetch().then((messages) => {
+        if (messages.size <= index - 1) {
+            //Send cooresponding message
+            channel.send(text).then(message => {
+                const reactionCopy = reactions;
+                addReactions(message, reactionCopy);
+            })
         } else {
-            member.roles.remove(role);
+            //Edit existing message
+            messageCount = messages.size - 1
+            // console.log(message);
+            // console.log(messages[index])
+            //message[1].edit(text);
+            for (const message of messages) {
+                console.log(messageCount);
+                console.log(message[messageCount]);
+                message[index].edit(text);
+                addReactions(message[index], reactions);
+                messageCount--;
+            }
         }
+        console.log(messages);
+        return '433';
+    });
+    /*
+    async execute(client, id, text, reactions = [], index) {
+        const channel = await client.channels.fetch(id);
 
-    }
-    let eText = `**======================================================**
-`;
-    indexPos = 0;
-    for (const key in emojiText) {
-        if (key.includes(':')) {
-            const split = key.split(':');
-            emojiName = split[1];
-            key = guild.emojis.cache.find((emoji) => {
-                return emojiName === emoji.name;
-            });
-        }
-        //console.log(key);
-        //const emoji = getEmoji(key.emote);
-        const emoji = reactions[indexPos++];
-        reactions.push(emoji)
-        const role = emojiText[key];
-        eText += `\n${emoji} <== ${role}\n`
-    }
-    const MessageID = messager(client, channelID, eText, reactions, index);
-    //2️⃣,3️⃣,4️⃣,5️⃣,6️⃣,7️⃣,8️⃣,9️⃣,🔟,🐦,🐕‍🦺,🎳,🐅,🇬,🇦
-    //1️⃣0️⃣
-    //'🇨🇳','🇩🇪','🇪🇸','🇯🇵','🇰🇷','🇵🇹','🇺🇸'
-
-    client.on('messageReactionAdd', (reaction, user) => {
-        if (reaction.message.channel.id === channelID) {
-            handelReaction(reaction, user, true);
-        }
-    })
-
-    client.on('messageReactionRemove', (reaction, user) => {
-        if (reaction.message.channel.id === channelID) {
-            handelReaction(reaction, user, false);
-        }
-    })
+        channel.messages.fetch().then((messages) => {
+            if (messages.size <= index - 1) {
+                //Send cooresponding message
+                channel.send(text).then(message => {
+                    addReactions(message, reactions);
+                })
+            } else {
+                //Edit existing message
+                messageCount = 0
+                for (const message of messages) {
+                    messageCount++;
+                    console.log(messageCount);
+                }
+            }
+        });
+    },
+    */
+   return 
 }
